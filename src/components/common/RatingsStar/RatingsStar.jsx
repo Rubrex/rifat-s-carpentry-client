dc3c0319c9a56443c5af90bb548b272e795127cc
Rubel Hossain <rubrex96@hotmail.com>
2022-11-10 07:41:09 +0600

    Login, Register, Footer Ui Updates

---
+++ src/components/common/RatingsStar/RatingsStar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const RatingsStar = () => {
+  // 5 stars
+  /*
+        5 Stars
+        ablitiy to choose stars
+        ability to set stars active
+
+    */
+  return <div></div>;
+};
+
+export default RatingsStar;
