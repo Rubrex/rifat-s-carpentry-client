d019377f7d5a8d689351af8a1ebaf767ee31eece
Rubel Hossain <rubrex96@hotmail.com>
2023-01-17 00:45:08 +0600

    updated header to sticky, added ScrollToTop component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -20,73 +20,75 @@ const Header = () => {
   };
 
   return (
-    <div className="container mx-auto ">
-      <Navbar fluid={true} rounded={true}>
-        <Link to="/" className="flex items-center">
-          <img src={logo} className="mr-3 h-14 md:h-20" alt="Flowbite Logo" />
-        </Link>
-        <div className="flex md:order-2">
-          {/* className="bg-red border border-orange-500 text-orange-500 hover:bg-orange-100 " */}
+    <div className="sticky top-0 z-50 bg-white">
+      <div className="container mx-auto ">
+        <Navbar fluid={true} rounded={true}>
+          <Link to="/" className="flex items-center">
+            <img src={logo} className="mr-3 h-14 md:h-20" alt="Flowbite Logo" />
+          </Link>
+          <div className="flex md:order-2">
+            {/* className="bg-red border border-orange-500 text-orange-500 hover:bg-orange-100 " */}
 
-          <div className="border-2 border-woodDark flex">
-            {user?.email ? (
-              <button
-                onClick={handleLogout}
-                className="bg-woodDark border-2 border-transparent  hover:bg-woodLight text-white py-1 px-4"
-                to="/register"
-              >
-                Logout
-              </button>
-            ) : (
-              <>
-                <NavLink
-                  className="bg-woodDark  border-2 border-transparent hover:bg-woodLight text-white py-1 px-4"
+            <div className="border-2 border-woodDark flex">
+              {user?.email ? (
+                <button
+                  onClick={handleLogout}
+                  className="bg-woodDark border-2 border-transparent  hover:bg-woodLight text-white py-1 px-4"
                   to="/register"
                 >
-                  Register
+                  Logout
+                </button>
+              ) : (
+                <>
+                  <NavLink
+                    className="bg-woodDark  border-2 border-transparent hover:bg-woodLight text-white py-1 px-4"
+                    to="/register"
+                  >
+                    Register
+                  </NavLink>
+                  <NavLink
+                    className=" hover:bg-woodLight border-2 border-transparent   text-woodDark py-1 px-4"
+                    to="/login"
+                  >
+                    Login
+                  </NavLink>
+                </>
+              )}
+            </div>
+            <Navbar.Toggle className="ml-5" />
+          </div>
+          <Navbar.Collapse className="bg-slate-200 md:bg-white p-5 md:p-0 ">
+            <NavLink
+              className="hover:text-woodDark text-woodLight  md:text-slate-800 mb-2 md:mb-0 py-1 px-4"
+              to="/services"
+            >
+              Services
+            </NavLink>
+            <NavLink
+              className="hover:text-woodDark text-woodLight  md:text-slate-800 mb-2 md:mb-0 py-1 px-4"
+              to="/blogs"
+            >
+              Blogs
+            </NavLink>
+            {user?.email && (
+              <>
+                <NavLink
+                  className="hover:text-woodDark text-woodLight  md:text-slate-800 mb-2 md:mb-0 py-1 px-4"
+                  to="/addservice"
+                >
+                  Add Service
                 </NavLink>
                 <NavLink
-                  className=" hover:bg-woodLight border-2 border-transparent   text-woodDark py-1 px-4"
-                  to="/login"
+                  className="hover:text-woodDark text-woodLight  md:text-slate-800 mb-2 md:mb-0 py-1 px-4"
+                  to="/myreviews"
                 >
-                  Login
+                  My Reviews
                 </NavLink>
               </>
             )}
-          </div>
-          <Navbar.Toggle className="ml-5" />
-        </div>
-        <Navbar.Collapse className="bg-slate-200 md:bg-white p-5 md:p-0 ">
-          <NavLink
-            className="hover:text-woodDark text-woodLight  md:text-slate-800 mb-2 md:mb-0 py-1 px-4"
-            to="/services"
-          >
-            Services
-          </NavLink>
-          <NavLink
-            className="hover:text-woodDark text-woodLight  md:text-slate-800 mb-2 md:mb-0 py-1 px-4"
-            to="/blogs"
-          >
-            Blogs
-          </NavLink>
-          {user?.email && (
-            <>
-              <NavLink
-                className="hover:text-woodDark text-woodLight  md:text-slate-800 mb-2 md:mb-0 py-1 px-4"
-                to="/addservice"
-              >
-                Add Service
-              </NavLink>
-              <NavLink
-                className="hover:text-woodDark text-woodLight  md:text-slate-800 mb-2 md:mb-0 py-1 px-4"
-                to="/myreviews"
-              >
-                My Reviews
-              </NavLink>
-            </>
-          )}
-        </Navbar.Collapse>
-      </Navbar>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
     </div>
   );
 };
